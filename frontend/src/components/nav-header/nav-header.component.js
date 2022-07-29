b8ebc6f80a73a2db5fe7e4c73c81f51988366aae
Mario Burgos <mario.burpi@gmail.com>
2022-07-29 18:52:28 +0200

    script muy simple de la bd

--- frontend/src/components/nav-header/nav-header.component.js
+++ frontend/src/components/nav-header/nav-header.component.js
@@ -1,6 +1,7 @@
 import { navHeader } from "../../data/constants/nav-header.component.constants.js";
 
 export class NavHeaderComponent {
+
   constructor(selector) {
     this.id = selector;
     this.parentDiv = document.querySelector(`${selector}`);
@@ -8,7 +9,11 @@ export class NavHeaderComponent {
 
   render() {
     let imgLogo = document.createElement("IMG");
+    let imgLogo2 = document.createElement("IMG");
+    imgLogo2.setAttribute("src", 'https://devconnected.com/wp-content/uploads/2020/02/git-branch.png');
+
     imgLogo.setAttribute("src", 'https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2147483647&v=beta&t=J3kUMZwIphc90TFKH5oOO9Sa9K59fimgJf-s_okU3zs');
+    this.parentDiv.appendChild(imgLogo2);
     return this.parentDiv.appendChild(imgLogo);
   }
 }
--- frontend/src/controllers/controller.js
+++ frontend/src/controllers/controller.js
@@ -1,8 +1,8 @@
 import { NavHeaderComponent } from "../components/nav-header/nav-header.component.js";
 
-document.addEventListener('onload', evt =>{
+document.addEventListener('click', evt =>{
     let parentDiv = document.querySelector('#nav-header');
-    let navHeader = new NavHeaderComponent('#nav-header');
+    let navHeader = new NavHeaderComponent('#nav-header', 'hola mundo');
     parentDiv = navHeader.render();
 })
 
