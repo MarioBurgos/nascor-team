94d181d0e39ab597eba472f085849f62c5074b51
niko1511 <niko1511@gmail.com>
2022-07-30 23:42:32 +0200

    inicio sección

--- frontend/src/controllers/controller.js
+++ frontend/src/controllers/controller.js
@@ -1,8 +1,30 @@
 import { NavHeaderComponent } from "../components/nav-header/nav-header.component.js";
+import { Usuario } from "../model/usuario.model.js";
+/*
+document.addEventListener("click", (evt) => {
+  let parentDiv = document.querySelector("#nav-header");
+  let navHeader = new NavHeaderComponent("#nav-header", "hola mundo");
+  parentDiv = navHeader.render();
+});
+*/
+// selecciona el formulario usuario y añade su contenido creado en la funcion render()
+let userLogin = new Usuario();
+userLogin.render("user-login");
 
-document.addEventListener('click', evt =>{
-    let parentDiv = document.querySelector('#nav-header');
-    let navHeader = new NavHeaderComponent('#nav-header', 'hola mundo');
-    parentDiv = navHeader.render();
-})
+//activa la escucha y define la funcion para cada button
+document.getElementById("signIn").addEventListener("click", signIn);
+document.getElementById("logIn").addEventListener("click", logIn);
+document.getElementById("logOut").addEventListener("click", logOut);
 
+function signIn() {
+  //code...
+  console.log(this);
+}
+function logIn() {
+  //code...
+  console.log(this);
+}
+function logOut() {
+  //code...
+  console.log(this);
+}
--- frontend/src/model/usuario.model.js
+++ frontend/src/model/usuario.model.js
@@ -1,13 +1,37 @@
-export class Usuario{
+export class Usuario {
+  constructor() {}
 
-    constructor(){
+  set passwd(e) {
+    this.email = e;
+  }
+  get passwd() {
+    return this.passwd;
+  }
 
-    }
+  render(selector) {
+    let parentForm = document.createElement("div"); //creamos un div
+    parentForm.id = `${selector}`; //y le agregamos el selector
+    // crea los campos a utilizar
+    let labelUser = document.createElement("label");
+    let labelPass = document.createElement("label");
 
-    set passwd(e){
-        this.email = e;
-    }
-    get passwd(){
-        return this.passwd;
-    }
+    //creamos un div y le añadimos 3 input submit
+    let divButtons = document.createElement("div");
+
+    // creamos y modificamos el contenido de los botones
+    divButtons.innerHTML = "<a href='#' id='signIn'>signIn</a>";
+    divButtons.innerHTML += "<a href='#' id='logIn'>logIn</a>";
+    divButtons.innerHTML += "<a href='#'  id='logOut'>logOut</a>";
+
+    // agrega el contenido a los campos creados
+    labelUser.innerHTML =
+      "Usuario<input type='text' name='user' placeholder='name'>";
+    labelPass.innerHTML =
+      "Password<input type='password' name='password' placeholder='password'>";
+    // añadimos al padre los campos
+    parentForm.appendChild(labelUser);
+    parentForm.appendChild(labelPass);
+    parentForm.appendChild(divButtons);
+    document.body.prepend(parentForm); // con prepend lo agregamos al body como primer hijo
+  }
 }
