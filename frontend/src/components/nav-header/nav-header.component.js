3bd41e825a3fbdb9a73dccca5722221c35c5bb25
Mario Burgos <mario.burpi@gmail.com>
2022-08-01 13:00:18 +0200

    nueva idea (hay comentarios) he conseguido inyectar codigo html desde el controller y desde la clase nav-header compnent.  ahora se pueden generar fragmentos de html de cada componente e inyectarlos en las páginas

--- frontend/src/components/nav-header/nav-header.component.js
+++ frontend/src/components/nav-header/nav-header.component.js
@@ -1,15 +1,20 @@
 import { constNavHeader } from "../../data/constants/nav-header.component.constants.js";
 
 export class NavHeaderComponent {
-
   constructor(selector) {
+    this.selector = selector;
     this.id = selector;
     this.parentDiv = document.querySelector(`${selector}`);
   }
 
   render() {
-    let imgLogo = document.createElement("IMG");
+    /**let imgLogo = document.createElement("IMG");
     imgLogo.setAttribute("src", constNavHeader.LOGOURL);
+    imgLogo.className = 'img-logo';
     return this.parentDiv.appendChild(imgLogo);
+    */
+    $(function () {
+      $("#nav-header").load("../../components/nav-header/nav-header.html");
+    });
   }
 }
--- frontend/src/controllers/controller.js
+++ frontend/src/controllers/controller.js
@@ -5,6 +5,8 @@ window.addEventListener('load', (evt) => {
   /**
    * crea el header
    */
+  
+  
   let parentDiv = document.querySelector("#nav-header");
   let navHeader = new NavHeaderComponent("#nav-header", "hola mundo");
   parentDiv = navHeader.render();
