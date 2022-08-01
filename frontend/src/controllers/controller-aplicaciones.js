78275ad7e4ffd0bb0a311269d8f1934fcfbfcfce
Mario Burgos <mario.burpi@gmail.com>
2022-08-01 14:10:58 +0200

    creo la estructura de páginas

---
+++ frontend/src/controllers/controller-aplicaciones.js
@@ -0,0 +1,13 @@
+import { NavHeaderComponent } from "../components/nav-header/nav-header.component.js";
+
+window.addEventListener('load', (evt) => {
+  /**
+   * crea el header
+   */
+  let parentDiv = document.querySelector("#nav-header");
+  let navHeader = new NavHeaderComponent("#nav-header", "hola mundo");
+  parentDiv = navHeader.render();
+
+});
+
+
